b0c65b0f20a755815db1802d50dbed747db08627
didericis <eric@dideric.is>
2025-09-26 13:52:18 -0400

    feat: add --format and --encoding options
    Copy pattern from bare-pack and infer appropriate format based on filename

--- bin.js
+++ bin.js
@@ -11,7 +11,7 @@ function readBundleFromFile(filePath) {
   return new Promise((resolve, reject) => {
     fs.readFile(filePath, (err, data) => {
       if (err) reject(err)
-      else resolve(Buffer.from(data))
+      else resolve(Bundle.from(data))
     })
   })
 }
@@ -32,17 +32,42 @@ function readBundleFromStdin() {
   })
 }
 
+function defaultFormat(out) {
+  if (typeof out !== 'string') return 'bundle'
+  if (out.endsWith('.bundle.js') || out.endsWith('.bundle.cjs'))
+    return 'bundle.cjs'
+  if (out.endsWith('.bundle.mjs')) return 'bundle.mjs'
+  if (out.endsWith('.bundle.json')) return 'bundle.json'
+  return 'bundle'
+}
+
 const cmd = command(
   pkg.name,
   summary(pkg.description),
   arg('[file]', 'File containing the bundle (will process stdin if omitted)'),
+  flag('--encoding|-e <name>', 'The encoding to use for text bundle formats'),
+  flag('--format|-f <name>', 'The bundle format to use'),
   flag('--version|-v', 'Print the current version'),
   flag('--plugin <path>', 'A list of plugins to run on the bundle').multiple(),
   flag('--out|-o <path>', 'The output path of the transformed bundle'),
   async (cmd) => {
+    const {
+      version,
+      plugin: pluginFiles,
+      out,
+      encoding = 'utf8',
+      format = defaultFormat(out)
+    } = cmd.flags
     const { file } = cmd.args
-    const { version, plugin: pluginFiles, out } = cmd.flags
+
     if (version) return console.log(`v${pkg.version}`)
+
+    // validate format option early so we can bail out if invalid
+    if (
+      !['bundle', 'bundle.cjs', 'bundle.mjs', 'bundle.json'].includes(format)
+    ) {
+      throw new Error(`Unknown format '${format}'`)
+    }
 
     const cwd = process.cwd()
 
@@ -63,12 +88,30 @@ const cmd = command(
       }
     }
 
+    let data = bundle.toBuffer()
+    switch (format) {
+      case 'bundle':
+        break
+      case 'bundle.cjs':
+        data = `module.exports = ${JSON.stringify(data.toString(encoding))}\n`
+        break
+      case 'bundle.mjs':
+        data = `export default ${JSON.stringify(data.toString(encoding))}\n`
+        break
+      case 'bundle.json':
+        data = JSON.stringify(data.toString(encoding)) + '\n'
+        break
+      default:
+        // NB: line will never be hit, kept for legibility/intent when refactoring
+        throw new Error(`Unknown format '${format}'`)
+    }
+
     if (out) {
       const url = pathToFileURL(out)
       await fs.promises.mkdir(new URL('.', url), { recursive: true })
-      await fs.promises.writeFile(url, bundle.toBuffer())
+      await fs.promises.writeFile(url, data)
     } else {
-      process.stdout.write(bundle.toBuffer())
+      process.stdout.write(data)
     }
   }
 )
